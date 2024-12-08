0c02228c62af831161129365a2884ca503399b42
jazzyengineermc <112543476+jazzyengineermc@users.noreply.github.com>
2024-12-07 23:18:26 -0500

    try this then

--- launch/imu_filter.launch.py
+++ launch/imu_filter.launch.py
@@ -8,7 +8,7 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    config_dir = os.path.join(get_package_share_directory('imu_filter_madgwick'), 'config')
+    config_dir = os.path.join(get_package_share_directory('raptor_robot_v2'), 'config')
 
     return launch.LaunchDescription(
         [
